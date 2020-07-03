6846bd6920dbde0e3f4fd7ef435f891381457007
Tshine <gtshine@gmail.com>
2020-07-03 16:15:28 +0800

    提交pub

--- event_bus_auto_codegen/lib/event_bus_auto_codegen.dart
+++ event_bus_auto_codegen/lib/event_bus_auto_codegen.dart
@@ -1,9 +1,4 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library event_bus_auto_codegen;
 
 export 'builder.dart';
 export 'src/event_bus_auto_generator.dart';
-
-// TODO: Export any libraries intended for clients of this package.
