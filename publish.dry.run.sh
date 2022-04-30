4aeb0f93a9ded3354e432c6a1d56409dcb360857
Tshine <gtshine@gmail.com>
2022-04-30 14:39:23 +0800

    update publish shell

--- publish.dry.run.sh
+++ publish.dry.run.sh
@@ -1,5 +1,6 @@
 cd event_bus_auto
-pub publish --dry-run
+dart pub publish --dry-run
+
 cd ..
 cd event_bus_auto_codegen
-pub publish --dry-run
+dart pub publish --dry-run
--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@ alias uncnflutter="unset PUB_HOSTED_URL;unset FLUTTER_STORAGE_BASE_URL"
 uncnflutter
 
 cd event_bus_auto
-pub publish
+dart pub publish
 cd ..
 cd event_bus_auto_codegen
-pub publish
+dart pub publish
